f5d1755e154653a3273d7272d3604c5eb0116ac3
Javier Godoy <rotvulpix@gmail.com>
2024-07-02 11:14:54 -0400

    wip: First attempt at charts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
-import { Flex } from 'antd';
+import { Flex, Row, Tooltip } from 'antd';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+
+const data: any[] = [];
+for (let i = 0; i < 13; i++) {
+  data.push({ name: `Page ${i}`, m: Math.random() * 5000, d: Math.random() * 5000, amt: Math.random() * 5000 });
+}
 
 const Home = () => {
   return (
@@ -6,7 +12,7 @@ const Home = () => {
       <header>
         <Flex justify='center'>
           <section>
-            <Flex justify='space-between'>
+            <Flex justify='space-between' className='header'>
               <div>logo + menu</div>
               <div>user menu</div>
             </Flex>
@@ -16,15 +22,28 @@ const Home = () => {
       <main>
         <Flex justify='center'>
           <section>
-            <iframe
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/r0qDBYiOBrc'
-              title='YouTube video player'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              referrerPolicy='strict-origin-when-cross-origin'
-              allowFullScreen
-            ></iframe>
+            <Row>
+              <ResponsiveContainer width='100%' height={300}>
+                <LineChart
+                  data={data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray='3 3' />
+                  <XAxis dataKey='name' />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line type='monotone' dataKey='m' name='Mobile' stroke='#8884d8' activeDot={{ r: 8 }}></Line>
+                  <Line type='monotone' dataKey='d' name='Desktop' stroke='#82ca9d' activeDot={{ r: 8 }} />
+                  <Line type='monotone' dataKey='amt' name='Totla' stroke='#82ca9d' activeDot={{ r: 8 }} />
+                </LineChart>
+              </ResponsiveContainer>
+            </Row>
           </section>
         </Flex>
       </main>
