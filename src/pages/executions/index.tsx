94ecb2b75abc842ec7c31684d71479b3d496ba43
Javier Godoy <rotvulpix@gmail.com>
2024-07-05 16:19:03 -0400

    wip: AGGrid is working

--- src/pages/executions/index.tsx
+++ src/pages/executions/index.tsx
@@ -2,25 +2,30 @@ import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import { Container } from '../../components/foundations/container';
 import { Stack } from '../../components/foundations/stack';
 import { Trigger } from '../../components/trigger';
-
+import { ColDef } from 'ag-grid-community';
 const ExecutionsList = () => {
-  // Row Data: The data to be displayed.
-  const rowData = [
+  type TData = {
+    make: string;
+    model: string;
+    price: number;
+    electric: boolean;
+  };
+
+  const rowData: TData[] = [
     { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
     { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
     { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
   ];
 
-  // Column Definitions: Defines the columns to be displayed.
-  const colDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }, { field: 'electric' }];
+  const colDefs: ColDef<TData>[] = [{ field: 'make' }, { field: 'model' }, { field: 'price' }, { field: 'electric' }];
 
   return (
     <Container>
       <Stack>
         <Trigger />
-        a
-        here goes the table
-        <AgGridReact rowData={rowData}  />b
+        <div className='ag-theme-quartz w-full' style={{ width: '100%', height: 500 }}>
+          <AgGridReact rowData={rowData} columnDefs={colDefs} />
+        </div>
       </Stack>
     </Container>
   );
