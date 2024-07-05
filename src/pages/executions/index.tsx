8c5f1c02af62f3ac5010bddd8bffa27a0a7def52
Javier Godoy <rotvulpix@gmail.com>
2024-07-05 18:03:18 -0400

    wip: Executions Table with mocked data

--- src/pages/executions/index.tsx
+++ src/pages/executions/index.tsx
@@ -3,28 +3,46 @@ import { Container } from '../../components/foundations/container';
 import { Stack } from '../../components/foundations/stack';
 import { Trigger } from '../../components/trigger';
 import { ColDef } from 'ag-grid-community';
+
 const ExecutionsList = () => {
   type TData = {
-    make: string;
-    model: string;
-    price: number;
-    electric: boolean;
+    UUID: string;
+    URL: string;
+    started: string;
+    status: string;
+    provider: string;
+    ended: string | null;
+    results: string;
   };
 
-  const rowData: TData[] = [
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-  ];
+  const data: TData[] = [];
+  for (let i = 0; i < 13; i++) {
+    data.push({
+      UUID: i.toString(),
+      URL: i % 4 == 0 ? 'https://www.clarin.com/' : 'https://www.lanacion.com.ar/',
+      started: new Date().toISOString(),
+      status: 'pending',
+      provider: i % 3 == 0 ? 'LH/ECS' : 'Google PSI',
+      ended: null,
+      results: 'link',
+    });
+  }
 
-  const colDefs: ColDef<TData>[] = [{ field: 'make' }, { field: 'model' }, { field: 'price' }, { field: 'electric' }];
+  const colDefs: ColDef<TData>[] = [
+    { field: 'UUID' },
+    { field: 'URL' },
+    { field: 'started' },
+    { field: 'status' },
+    { field: 'provider' },
+    { field: 'results' },
+  ];
 
   return (
     <Container>
       <Stack>
         <Trigger />
         <div className='ag-theme-quartz w-full' style={{ width: '100%', height: 500 }}>
-          <AgGridReact rowData={rowData} columnDefs={colDefs} />
+          <AgGridReact rowData={data} columnDefs={colDefs} />
         </div>
       </Stack>
     </Container>
