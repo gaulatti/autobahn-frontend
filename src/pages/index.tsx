a37ca2a42cb6aebfa4324e30d1d18197b447c53c
Javier Godoy <rotvulpix@gmail.com>
2024-07-05 20:49:22 -0400

    wip: Radar Chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,51 @@ import { DatePicker } from '@fluentui/react-datepicker-compat';
 import { HistoryScoreChart } from '../components/charts';
 import { Container } from '../components/foundations/container';
 import { Stack } from '../components/foundations/stack';
+import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend, ResponsiveContainer } from 'recharts';
 const Home = () => {
+  const data = [
+    {
+      "subject": "Homepage",
+      "LCP": 150,
+      "FCP": 110,
+      "fullMark": 200
+    },
+    {
+      "subject": "Content",
+      "LCP": 199,
+      "FCP": 130,
+      "fullMark": 200
+    },
+    {
+      "subject": "About Us",
+      "LCP": 186,
+      "FCP": 130,
+      "fullMark": 200
+    },
+    {
+      "subject": "Media Player",
+      "LCP": 169,
+      "FCP": 100,
+      "fullMark": 200
+    },
+    {
+      "subject": "Gallery",
+      "LCP": 185,
+      "FCP": 90,
+      "fullMark": 200
+    },
+    {
+      "subject": "History",
+      "LCP": 85,
+      "FCP": 45,
+      "fullMark": 200
+    }
+  ]
   return (
     <Container>
       <Stack>
         <div className='flex flex-wrap w-full my-4'>
-        <div className='flex-1 min-w-[100%] md:min-w-[50%] lg:min-w-[25%]'>
+          <div className='flex-1 min-w-[100%] md:min-w-[50%] lg:min-w-[25%]'>
             <Field label='Metric'>
               <Select id='metric-select'>
                 <option>Score</option>
@@ -46,7 +85,18 @@ const Home = () => {
         <HistoryScoreChart />
         <Divider>Detailed Stats</Divider>
         <div className='flex flex-wrap w-full my-4'>
-          <div className='flex-1 min-w-[50%] md:min-w-[25%]'>Radial: Executions per Score range</div>
+          <div className='flex-1 min-w-[50%] md:min-w-[25%]'>
+            <ResponsiveContainer width='100%' height={300} className='m-4'>
+              <RadarChart outerRadius={90} width={730} height={250} data={data}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey='subject' />
+                <PolarRadiusAxis angle={30} domain={[0, 200]} />
+                <Radar name='FCP' dataKey='FCP' stroke='#8884d8' fill='#8884d8' fillOpacity={0.6} />
+                <Radar name='LCP' dataKey='LCP' stroke='#82ca9d' fill='#82ca9d' fillOpacity={0.6} />
+                <Legend />
+              </RadarChart>
+            </ResponsiveContainer>
+          </div>
           <div className='flex-1 min-w-[50%] md:min-w-[25%]'>Column 2</div>
           <div className='flex-1 min-w-[50%] md:min-w-[25%]'>Column 3</div>
           <div className='flex-1 min-w-[50%] md:min-w-[25%]'>Column 4</div>
