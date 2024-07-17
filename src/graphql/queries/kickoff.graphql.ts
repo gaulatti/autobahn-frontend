89d551b3aff0371382040c7a82a127c32c3d9dc5
Javier Godoy Núñez <javier@oxozon.io>
2024-07-17 12:40:38 -0400

    fix: Restoring enums, improvements on logoutgp

--- src/graphql/queries/kickoff.graphql.ts
+++ src/graphql/queries/kickoff.graphql.ts
@@ -23,7 +23,7 @@ const getKickoff = gql`
           }
         }
       }
-          enums
+      enums
     }
   }
 `;
--- src/pages/auth/logout.tsx
+++ src/pages/auth/logout.tsx
@@ -9,6 +9,8 @@ const Logout = () => {
   logout();
 
   navigate('/');
+
+  return <></>
 };
 
 export { Logout };
