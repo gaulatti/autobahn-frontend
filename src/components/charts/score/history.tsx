798e92adb706993fe9dfe462330a433361f2c3b9
Javier Godoy Núñez <javier@oxozon.io>
2024-08-07 19:09:36 -0400

    wip: Server Side pagination/sorting, improvements on charts

--- src/components/charts/score/history.tsx
+++ src/components/charts/score/history.tsx
@@ -1,27 +1,30 @@
 import { Title3 } from '@fluentui/react-components';
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-const HistoryScoreChart = ({ data }: { data?: any[] }) => (
-  <ResponsiveContainer width='100%' height={300} className='m-4'>
-    <LineChart
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 30,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' />
-      <YAxis domain={[0, 'auto']}/>
-      <Tooltip />
-      <Legend />
-      <Title3>Performance Score</Title3>
-      <Line type='monotone' isAnimationActive={false} dataKey='m' name='Mobile' stroke='#d35400' activeDot={{ r: 8 }}></Line>
-      <Line type='monotone' isAnimationActive={false} dataKey='d' name='Desktop' stroke='#2980b9' activeDot={{ r: 8 }} />
-    </LineChart>
-  </ResponsiveContainer>
-);
+const HistoryScoreChart = ({ data }: { data?: any[] }) => {
+
+  return (
+    <ResponsiveContainer width='100%' height={300} className='m-4'>
+      <LineChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 30,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='name' />
+        <YAxis domain={[0, 'auto']} />
+        <Tooltip />
+        <Legend />
+        <Title3>Performance Score</Title3>
+        <Line type='monotone' isAnimationActive={false} dataKey='m' name='Mobile' stroke='#d35400' strokeWidth='3' activeDot={{ r: 8 }} />
+        <Line type='monotone' isAnimationActive={false} dataKey='d' name='Desktop' stroke='#2980b9' strokeWidth='3' activeDot={{ r: 8 }} />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
 export { HistoryScoreChart };
--- src/pages/executions/index.tsx
+++ src/pages/executions/index.tsx
@@ -1,16 +1,11 @@
-import {
-  Breadcrumb,
-  BreadcrumbDivider,
-  BreadcrumbItem,
-  Title1
-} from '@fluentui/react-components';
-import { ColDef } from 'ag-grid-community';
+import { Breadcrumb, BreadcrumbDivider, BreadcrumbItem, Spinner, Title1 } from '@fluentui/react-components';
+import { ColDef, IDatasource, IGetRowsParams, ValueGetterParams } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
 import moment from 'moment';
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Method, useAPI } from '../../clients/api';
+import { Method, sendRequest } from '../../clients/api';
 import { Container } from '../../components/foundations/container';
 import { Stack } from '../../components/foundations/stack';
 import { Trigger } from '../../components/trigger';
@@ -25,32 +20,95 @@ type TData = {
   results: string;
 };
 
+/**
+ * Colors are matching the ENUM values from the backend
+ */
+const semaphoreColors = ['#7f8c8d', '#f6b93b', '#27ae60', '#27ae60', '#27ae60', '#c0392b'];
+
 const ExecutionsList = () => {
   const enums = useSelector(getEnums);
   const [refresh, setRefresh] = useState<number>(0);
-  const { loading, data } = useAPI(Method.GET, [refresh], 'executions');
+  const gridRef = useRef<AgGridReact>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    gridRef.current?.api?.refreshCells();
+  }, [refresh]);
 
   const colDefs: ColDef<TData>[] = [
-    { field: 'uuid', headerName: 'UUID', flex: 1 },
-    { field: 'url', headerName: 'URL', flex: 1 },
-    { field: 'status', headerName: 'Status', flex: 1, cellRenderer: (params: { data: TData }) => enums.beaconStatus[params.data.status] },
-    { field: 'mode', headerName: 'Mode', flex: 1, cellRenderer: (params: { data: TData }) => enums.viewportMode[params.data.mode] },
-    { field: 'created_at', headerName: 'Triggered', flex: 1, cellRenderer: (params: { data: TData }) => moment(params.data.created_at).fromNow() },
+    {
+      field: 'uuid',
+      headerName: 'UUID',
+      cellStyle: (params) => {
+        const color = params.data && semaphoreColors[params.data!.status];
+        return { borderLeftColor: color || '', borderLeftWidth: '5px', borderLeftStyle: 'solid' };
+      },
+    },
+    { field: 'url', headerName: 'URL', flex: 1, filter: true },
+    {
+      field: 'status',
+      headerName: 'Status',
+      valueGetter: (params: ValueGetterParams<TData, any>) => params.data && enums.beaconStatus[params.data.status],
+      filter: true,
+      cellStyle: { display: 'flex' },
+    },
+    {
+      field: 'mode',
+      headerName: 'Mode',
+      flex: 1,
+      valueGetter: (params: ValueGetterParams<TData, any>) => params.data && enums.viewportMode[params.data.mode],
+      filter: true,
+    },
+    {
+      field: 'created_at',
+      headerName: 'Triggered',
+      cellRenderer: (params: { data: TData }) => {
+        return params.data && moment(params.data!.created_at).fromNow();
+      },
+      filter: 'agDateColumnFilter',
+      initialSort: 'desc',
+      sortingOrder: ['desc', 'asc'],
+    },
     {
       field: 'results',
       headerName: 'Actions',
-      flex: 1,
-      cellRenderer: (params: { data: TData }) => <b><Link to={`/executions/${params.data.uuid}`}>View Results</Link></b>,
+      pinned: 'right',
+      cellRenderer: (params: { data: TData }) =>
+        params.data &&
+        (params.data.status === 0 ? (
+          <Spinner size='extra-tiny' />
+        ) : (
+          <b>
+            <Link to={`/executions/${params.data?.uuid}`}>View Results</Link>
+          </b>
+        )),
+      cellStyle: { display: 'flex', alignItems: 'center', justifyContent: 'center' },
+      sortable: false,
     },
   ];
+
+  const dataSource: IDatasource = useMemo(
+    () => ({
+      rowCount: undefined,
+
+      getRows: async (params: IGetRowsParams) => {
+        console.log(params);
+        setLoading(true);
+        const rowsThisPage = await sendRequest(Method.GET, 'executions');
+        params.successCallback(rowsThisPage, -1);
+        setLoading(false);
+      },
+    }),
+    []
+  );
 
   return (
     <Container>
       <Stack>
         <Title1 className='text-left'>Beacon Executions</Title1>
-        <Breadcrumb aria-label='Breadcrumb default example'>
+        <Breadcrumb>
           <BreadcrumbItem>
-              <Link to='/'>Home</Link>
+            <Link to='/'>Home</Link>
           </BreadcrumbItem>
           <BreadcrumbDivider />
           <BreadcrumbItem>
@@ -59,7 +117,16 @@ const ExecutionsList = () => {
         </Breadcrumb>
         <Trigger setRefresh={setRefresh} />
         <div className='ag-theme-quartz w-full' style={{ width: '100%', height: 500 }}>
-          <AgGridReact loading={loading} rowData={data} columnDefs={colDefs} pagination={true} paginationPageSize={20} />
+          <AgGridReact
+            datasource={dataSource}
+            ref={gridRef}
+            rowModelType='infinite'
+            loading={loading}
+            columnDefs={colDefs}
+            pagination={true}
+            paginationPageSize={20}
+            sortingOrder={['desc', 'asc', null]}
+          />
         </div>
       </Stack>
     </Container>
